9523c9adf63164213507c531f0985f92676c823c
Shashwat Mahajan <shashwatmahajan2@gmail.com>
2026-03-23 04:05:53 +0530

    fix routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,60 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useNavigate,
+  Navigate,
 } from "react-router-dom";
+
 import Login from "./pages/login";
 import RollingQR from "./pages/Teacher";
 import Student from "./pages/Student";
 import Admin from "./components/Admin";
 
 const ProtectedRoute = ({ children }) => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const user = localStorage.getItem("loggedInUser");
-    if (!user) {
-      navigate("/", { replace: true });
-    }
-  }, [navigate]);
+  const user = localStorage.getItem("loggedInUser");
+
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
 
   return children;
 };
 
-
 const App = () => {
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route
-            path="/student"
-            element={
-              <div className="min-h-screen bg-gray-100 font-sans flex flex-col">
-                <Student />
-              </div>
-            }
-          />
-          <Route
-            path="/teacher"
-            element={
-              <ProtectedRoute>
-                <RollingQR />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route
+          path="/student"
+          element={
+            <ProtectedRoute>
+              <Student />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/teacher"
+          element={
+            <ProtectedRoute>
+              <RollingQR />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
